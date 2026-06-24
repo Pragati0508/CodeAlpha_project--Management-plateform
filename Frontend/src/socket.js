9dc91d3d451d4f00e1e9277754e05a2c17fb7002
pragati0508 <2k23.csai2311177@gmail.com>
2026-06-24 13:55:12 +0530

    Connected frontend to deployed backend

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "http://localhost:5000"
+  "https://codealpha-project-management-plateform.onrender.com",
+  {
+    transports: ["websocket"],
+  }
 );
 
 export default socket;
